8c1d708cf2ae56d5ab3c1e49649af43b8ce40053
Kayetic <bartek.bak@protonmail.com>
2022-11-08 21:03:51 +0000

    some MORE changes

--- misc-data/main.py
+++ misc-data/main.py
@@ -1,7 +1,1 @@
-last_car_mileage = int(input("Enter the mileage since last time car was filled: "))
-current_car_mileage = int(input("Enter the current mileage of the car: "))
-covered_mileage = current_car_mileage - last_car_mileage
-total_litres = int(input("Enter the total litres taken to fill the tank: "))
-gallons = total_litres / 4.546
-miles_per_gallon = covered_mileage / gallons
-print("The miles per gallon is: ", miles_per_gallon)
+
