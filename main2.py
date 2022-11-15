d63268322200254f52c463441cbce3d91ae3f534
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-11-15 09:52:39 +0000

    task 3 question 5b

--- main2.py
+++ main2.py
@@ -1,19 +1,3 @@
-attendance = int(input("Enter your attendance percentage: "))
-mark = int(input("Enter your exam mark: "))
-fail = False
-if attendance > 90:
-    if mark > 90:
-        grade = "A"
-    elif mark > 80 and mark <= 90:
-        grade = "B"
-    elif mark > 70 and mark <= 80:
-        grade = "C"
-    elif mark <= 70:
-        fail = True
-    if fail:
-        print("Failed")
-    else:
-        print("Grade: ", grade)
-else:
-    fail = True
-    print("Failed")
+if trigger == True:
+    if movementGround == True or movementFirst == True:
+        print("Intruder alert!")
