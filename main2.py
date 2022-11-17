658d8bb3a4c9c600f2323d98fd565418c27956e6
Kayetic <bartek.bak@protonmail.com>
2022-11-17 10:10:20 +0000

    another vsv task

--- main2.py
+++ main2.py
@@ -1,10 +1,6 @@
-x = int(input("Enter the first integer: "))
-y = int(input("Enter the second integer: "))
-z = 0
-while x > 0:
-    if x % 2 == 1:
-        z = z + y
-    x = x / 2
-    y = y * 2
-
-print("Answer =", z)
+while True:
+    part_number = int(input("Enter a number: "))
+    if len(str(part_number)) != 4:
+        print("Enter a 4 digit number")
+    else:
+        break
