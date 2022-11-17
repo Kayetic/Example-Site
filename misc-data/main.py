72b6476c96383b93dab00940a1f0052196a361a1
Kayetic <bartek.bak@protonmail.com>
2022-11-17 16:24:19 +0000

    yet more vsv work

--- misc-data/main.py
+++ misc-data/main.py
@@ -1,4 +1,18 @@
-temp = input(“Enter the temperature”)
-humidity = input(“Enter the humidity”)
-if window == “closed”:
-    
+total = 0
+old = 0
+
+while True:
+    while True:
+        part_number = input("Enter part number: ")
+        if len(part_number) != 4:
+            print("Invalid part number. Must be 4 characters long.")
+        else:
+            break
+    if part_number == "9999":
+        break
+    total += 1
+    if part_number[3] == "6" or part_number[3] == "7" or part_number[3] == "8":
+        old += 1
+
+print("Total parts: ", total)
+print("Old parts: ", old)
