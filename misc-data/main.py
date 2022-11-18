a89ef8cafe140a4771c156dd98f887849edc6d58
Kayetic <bartek.bak@protonmail.com>
2022-11-18 18:55:22 +0000

    yay

--- misc-data/main.py
+++ misc-data/main.py
@@ -1,15 +1,10 @@
-total = 0
-average_scores = []
-for i in range(30):
-    student_score = 0
-    for j in range(3):
-        score = int(input(f"Enter the student's score for test {j+1}: "))
-        student_score += score
-    average = round(student_score / 3, 2)
-    print(f"The student's average score is {average}")
-    average_scores.append(average)
-    total += student_score
-print(f"The class average is {total/90:.2f}")
-print("Individual student scores:")
-for i in range(len(average_scores)):
-    print(f"Student {i+1}: {average_scores[i]}")
+x = 1
+password = "Tues1212"
+while x < 3:
+    user_password = input("Enter password: ")
+    if user_password == password:
+        print("Password accepted")
+        break
+    else:
+        print("Password rejected")
+        x += 1
