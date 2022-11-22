a4e2bcb05c7c50ec34c4ee94d66f83fccd9c7dba
Kayetic <bartek.bak@protonmail.com>
2022-11-22 18:31:36 +0000

    yet another vsv task

--- main2.py
+++ main2.py
@@ -1,23 +1,4 @@
-def getPword(attempt):
-    if attempt == 1:
-        user_password = input("Enter your password: ")
-        if len(user_password) < 6 or len(user_password) > 8:
-            print("Password must be at least 8 characters long.")
-            return getPword(1)
-    elif attempt == 2:
-        if len(user_password) < 6 or len(user_password) > 8:
-            print("Password must be at least 8 characters long.")
-            return getPword(2)
-        user_password = input("Enter the password again: ")
-    return user_password
-
-
-def main():
-    user_password = getPword(1)
-    if user_password == getPword(2):
-        print("Password accepted.")
-    else:
-        print("Password does not match.")
-
-
-main()
+def parkACar():
+    reg = input("Enter the registration number of your vehicle: ")
+    row = input("Choose a row: ")
+    column = input("Choose a column")
