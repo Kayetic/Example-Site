05d983cb45584f8dfd7b7b012da0f628d58826ea
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-11-22 09:37:14 +0000

    yet another vsv task

--- main2.py
+++ main2.py
@@ -1,15 +1,5 @@
-total = 0
-average_scores = []
-for i in range(30):
-    student_score = 0
-    for j in range(3):
-        score = int(input(f"Enter the student's score for test {j+1}: "))
-        student_score += score
-    average = round(student_score / 3, 2)
-    print(f"The student's average score is {average}")
-    average_scores.append(average)
-    total += student_score
-print(f"The class average is {round(total/90), 2}")
-print("Individual student scores:")
-for i in range(len(average_scores)):
-    print(f"Student {i+1}: {average_scores[i]}")
+letters = []
+for i in range(5):
+    letters.append(ord(input("Enter a letter: ")))
+print("The largest letter is", chr(max(letters)))
+print("The smallest letter is", chr(min(letters)))
