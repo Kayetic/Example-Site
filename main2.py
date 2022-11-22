5b3725cc1766c5ee2ef605c1af7ecc5e6f13287f
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-11-22 10:50:02 +0000

    not sure what this is tbh

--- main2.py
+++ main2.py
@@ -1,5 +1,16 @@
-letters = []
-for i in range(5):
-    letters.append(ord(input("Enter a letter: ")))
-print("The largest letter is", chr(max(letters)))
-print("The smallest letter is", chr(min(letters)))
+
+def multiples(table, startnum, endnum):
+    for i in range(startnum, endnum + 1):
+        print(table, "x", i, "=", table * i)
+    print()
+
+
+# main program
+name = input("What is your name?\n")
+print("Enter times table, start number and end number")
+user_table = int(input())
+user_startnum = int(input())
+user_endnum = int(input())
+
+print(f"Hi {name} ... here is your times table")
+multiples(user_table, user_startnum, user_endnum)
