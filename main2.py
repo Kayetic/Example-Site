4283f2a072182cfb71902a252427a0d879dbc57d
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-11-22 09:31:44 +0000

    another vsv task

--- main2.py
+++ main2.py
@@ -1,6 +1,15 @@
-while True:
-    part_number = int(input("Enter a number: "))
-    if len(str(part_number)) != 4:
-        print("Enter a 4 digit number")
-    else:
-        break
+total = 0
+average_scores = []
+for i in range(30):
+    student_score = 0
+    for j in range(3):
+        score = int(input(f"Enter the student's score for test {j+1}: "))
+        student_score += score
+    average = round(student_score / 3, 2)
+    print(f"The student's average score is {average}")
+    average_scores.append(average)
+    total += student_score
+print(f"The class average is {total/90:.2f}")
+print("Individual student scores:")
+for i in range(len(average_scores)):
+    print(f"Student {i+1}: {average_scores[i]}")
