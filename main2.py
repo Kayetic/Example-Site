02a7f5d959dca2fc5e6eb4215ed6a7a842f07c27
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-12-01 10:03:47 +0000

    a misc task for vsv

--- main2.py
+++ main2.py
@@ -1,37 +1,9 @@
-names = []
+with open('data.txt') as original_file:
+    data = original_file.read()
 
+print(data)
 
-def addName():
-    name = input("Enter the name to be added to the list: ")
-    position = int(input("Enter the position to add the name: "))
-    names.insert(position, name)
-
-
-def listNames():
-    print("The names in the list are: ")
-    for name in names:
-        print(names.index(name), name)
-
-
-def displayMenu():
-    while True:
-        choice = input("""
-1 Add name
-2 Display list
-3 Quit
-: """)
-        if choice != "1" and choice != "2" and choice != "3":
-            print("Invalid choice")
-            continue
-        else:
-            return choice
-
-
-choice = displayMenu()
-if choice == "1":
-    addName()
-elif choice == "2":
-    listNames()
-elif choice == "3":
-    print("Program Terminating")
-    exit()
+with open('word_by_word.txt', 'w') as word_by_word_file:
+    for word in data.split():
+        word_by_word_file.write(word + '\n')
+    word_by_word_file.close()
