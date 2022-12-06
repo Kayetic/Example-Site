442998e00114ddb7fb0a8d9f660fac8be489403b
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-12-06 09:34:20 +0000

    Not sure what changed

--- main2.py
+++ main2.py
@@ -1,9 +1,16 @@
-with open('data.txt') as original_file:
-    data = original_file.read()
+import os
+import openai
 
-print(data)
+openai.api_key = "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"
 
-with open('word_by_word.txt', 'w') as word_by_word_file:
-    for word in data.split():
-        word_by_word_file.write(word + '\n')
-    word_by_word_file.close()
+response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Explain hashing with an example",
+    temperature=0,
+    max_tokens=3000,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
+
+print(response["choices"][0]["text"])
