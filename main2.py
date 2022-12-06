8743d15fe1557dffd3c24991192e0bee71322681
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-12-06 10:10:31 +0000

    not working vsv task

--- main2.py
+++ main2.py
@@ -1,20 +1,30 @@
-# Description: Fibonacci sequence
-
+# Non-Recursive function
 def fibonacci(n):
+    print("Fibonacci sequence:")
+    a = 0
+    b = 0
+    while b <= n:
+        c = a + b
+        a = b
+        b = c
+        print(b)
+
+
+fibonacci(10)
+
+
+# Recursive function
+def fibonacci_recursive(n):
     if n <= 1:
         return n
     else:
-        return (fibonacci(n-1) + fibonacci(n-2))
-
-# main function
+        return (fibonacci_recursive(n-1) + fibonacci_recursive(n-2))
 
 
-def main():
-    n = int(input("Enter a number: "))
-    print("Fibonacci sequence:")
-    for i in range(n):
-        print(fibonacci(i))
-
-
-# call main function
-main()
+def sum_even(n):
+    if n == 0:
+        return 0
+    elif n % 2 == 0:
+        return n + sum_even(n-2)
+    else:
+        return sum_even(n-1)
