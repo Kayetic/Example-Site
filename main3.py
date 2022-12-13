db8f45278efe7483f1cf21827c1d479c6eb7cf82
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-12-13 09:21:23 +0000

    factorial function

---
+++ main3.py
@@ -0,0 +1,14 @@
+# factorial of a number
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
+
+
+def main():
+    print(fact(27))
+
+
+if __name__ == "__main__":
+    main()
