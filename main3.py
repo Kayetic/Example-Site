9c40489ea77f0ca07b25254d75cbfd0829db2e42
Bartek Bak <bartek.bak@protonmail.com>
2022-12-15 09:51:45 +0000

    playing with oop

---
+++ main3.py
@@ -0,0 +1,13 @@
+class Person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def drive():
+        print("Driving")
+    def age(years):
+        print(f"I was {age} years old before...")
+        print(f"I am now {age + int(years)} years old.")
+
+
+person1 = Person("John", "32")
+person1.age(int(5))
