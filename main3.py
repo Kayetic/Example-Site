adf22db68973640c69d86ca793ecb9b1432b5c10
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-12-15 09:55:32 +0000

    more and MORE oop

--- main3.py
+++ main3.py
@@ -2,13 +2,23 @@ class Person():
     def __init__(self, name, age):
         self.name = name
         self.age = age
-    def drive():
+
+    def drive(self):
         print("Driving")
+
     def set_age(self, age):
         self.age = age
-        # print(f"I was {age} years old before...")
-        # print(f"I am now {age + int(years)} years old.")
+
+    def get_age(self):
+        print(f"I am now {self.age} years old")
+
+    def get_older(self, amount):
+        self.age += amount
+        print(f"I am now {self.age} years old")
 
 
-person1 = Person("John", "32")
-person1.age(int(5))
+person1 = Person("John", 36)
+person1.drive()
+person1.set_age(40)
+person1.get_age()
+person1.get_older(5)
