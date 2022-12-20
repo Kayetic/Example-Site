1e8828da078181809df548dc7f9f153bce252e40
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-12-20 09:46:12 +0000

    added a recursive fibbonacci function

--- main2.py
+++ main2.py
@@ -17,6 +17,21 @@ def fibonacci_recursive(n):
     else:
         return (fibonacci_recursive(n-1) + fibonacci_recursive(n-2))
 
+# Non-Recursive function
+
+
+def fibonnaci_non_recursive(n):
+    if n <= 1:
+        return n
+    else:
+        a = 0
+        b = 1
+        for i in range(2, n+1):
+            c = a + b
+            a = b
+            b = c
+        return b
+
 
 def sum_even(n):
     if n == 0:
