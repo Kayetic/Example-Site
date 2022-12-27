6362079db931c7a664173c707c9e964786a6cb47
Kayetic <bartek.bak@protonmail.com>
2022-12-27 17:06:47 +0000

    bruh learned some flexbox stuff

--- main3.py
+++ main3.py
@@ -1,24 +1,15 @@
-class Person():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# 1, 3, 3, 9, 12
+# target = 6
 
-    def drive(self):
-        print("Driving")
-
-    def set_age(self, age):
-        self.age = age
-
-    def get_age(self):
-        print(f"I am now {self.age} years old")
-
-    def get_older(self, amount):
-        self.age += amount
-        print(f"I am now {self.age} years old")
-
-
-person1 = Person("John", 36)
-person1.drive()
-person1.set_age(40)
-person1.get_age()
-person1.get_older(5)
+array = [1, 3, 3, 9, 12]
+target = int(input('Enter a target number: '))
+for firstNumber in array:
+    if firstNumber >= target:
+        continue
+    else:
+        for secondNumber in array:
+            if firstNumber + secondNumber == target:
+                print(
+                    f'Done, indexes: {array.index(firstNumber)}, {array.index(secondNumber)}')
+            else:
+                continue
