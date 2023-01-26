b3650d3b0b971be93d86aa11e3c734783e73dadf
Kayetic <bartek.bak@protonmail.com>
2023-01-26 20:49:02 +0000

    another problem

--- main3.py
+++ main3.py
@@ -1,17 +1,9 @@
 # problem:
 
 # 1. Create a function that takes a list of numbers and returns the sum of the two lowest positive numbers.
-# sort list, check if number is >= 0, sum them
 
 def sumPositive(list):
-    list.sort()
-    for number in list:
-        print(number)
-        if number < 0:
-            removed = list.pop(number)
-        else:
-            continue
-    return list
+    ''''''
 
 
 list = [1, 4, 12, 5, -1, -9, 0, 13, 55, 2, -123]
