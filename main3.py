af99c6724faf3ab45a43a91de2998115dfb12e86
Kayetic <bartek.bak@protonmail.com>
2023-01-26 19:01:08 +0000

    writing problem

--- main3.py
+++ main3.py
@@ -1,43 +1,18 @@
-class Dog():
-    def __init__(self, dogName, myColour):
-        self.__name = dogName
-        self.__colour = myColour
+# problem:
 
-    def bark(self, barkTimes):
-        for n in range(barkTimes):
-            print("Woof!")
+# 1. Create a function that takes a list of numbers and returns the sum of the two lowest positive numbers.
+# sort list, check if number is >= 0, sum them
 
-    def setColor(self, myColour):
-        self.__colour = myColour
-
-    def getColour(self):
-        return self.__colour
-
-    def getName(self):
-        return self.__name
-
-    def printDogDetails(self):
-        print(self.__name, self.__colour)
+def sumPositive(list):
+    list.sort()
+    for number in list:
+        print(number)
+        if number < 0:
+            removed = list.pop(number)
+        else:
+            continue
+    return list
 
 
-class Puppy(Dog):
-    def __init__(self, dogName, myColour):
-        super().__init__(dogName, myColour)
-
-    __shoesChewed = 0
-
-    def chewShoe(self, myShoesChewed):
-        self.__shoesChewed += myShoesChewed
-
-    def getShoesChewed(self):
-        return self.__shoesChewed
-
-    # The super() function allows you to call a method from the parent class
-    # In this case, the bark() method from the Dog class
-
-    def bark(self, barkTimes):
-        return super().bark(barkTimes)
-
-
-puppy1 = Puppy("Fido", "Brown")
-puppy1.bark(3)
+list = [1, 4, 12, 5, -1, -9, 0, 13, 55, 2, -123]
+print(sumPositive(list))
