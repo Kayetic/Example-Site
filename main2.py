ceab02ad6140c59334cf12e0434970dd3fd4275a
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2023-03-09 09:36:40 +0000

    made a whisper program

--- main2.py
+++ main2.py
@@ -1,42 +1,13 @@
-# Non-Recursive function
-def fibonacci(n):
-    print("Fibonacci sequence:")
-    a = 0
-    b = 0
-    while b <= n:
-        c = a + b
-        a = b
-        b = c
-        print(b)
+import whisper
+
+model = whisper.load_model("base")
+
+result = model.transcribe("audio.mp3")
+print(result["text"])
 
 
-# Recursive function
-def fibonacci_recursive(n):
-    if n <= 1:
-        return n
-    else:
-        return (fibonacci_recursive(n-1) + fibonacci_recursive(n-2))
+with open("transcription.txt", "w") as f:
+    f.write(result["text"])
+    f.close()
 
-# Non-Recursive function
-
-
-def fibonnaci_non_recursive(n):
-    if n <= 1:
-        return n
-    else:
-        a = 0
-        b = 1
-        for i in range(2, n+1):
-            c = a + b
-            a = b
-            b = c
-        return b
-
-
-def sum_even(n):
-    if n == 0:
-        return 0
-    elif n % 2 == 0:
-        return n + sum_even(n-2)
-    else:
-        return sum_even(n-1)
+# upload the transcription to your google drive
