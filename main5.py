28b13e00622981dadad0915a640bd1eb73a94f5e
Kayetic <bartek.bak@protonmail.com>
2023-03-29 17:51:26 +0100

    🎉 feat(main5.py): add pyautogui script to press down arrow 25 times with a delay of 0.2 seconds between each press and delay of 3 seconds between each set of 25 presses

--- main5.py
+++ main5.py
@@ -1,1 +1,16 @@
-print("Heyy")
+import pyautogui
+import time
+
+# write me a pyautogui script that will press the down arrow 25 times with a delay of 0.2 seconds between each press and delay of 3 seconds between each set of 25 presses
+
+pyautogui.PAUSE = 0.2
+pyautogui.FAILSAFE = True
+
+time.sleep(5)
+for i in range(12):
+    for i in range(25):
+        pyautogui.press('down')
+        print(i)
+        if i == 24:
+            pyautogui.sleep(3)
+            i = 0
