6f3df3faa493b4c78303716bacd24b56492ef77e
Kayetic <bartek.bak@protonmail.com>
2023-03-29 09:51:35 +0100

    🔥 refactor(main5.py): remove unused code and comments

--- main5.py
+++ main5.py
@@ -11,30 +11,3 @@ def fib(n):
 
 
 fib(2000)
-
-
-# write a fibbonaci function recursive
-
-def fib2(n):
-    if n < 2:
-        return n
-    return fib2(n-2) + fib2(n-1)
-
-
-print(fib2(10))
-
-
-# write a fibbonaci function recursive with memoization
-
-def fib3(n, memo={}):
-    if n < 2:
-        return n
-    if n not in memo:
-        memo[n] = fib3(n-2, memo) + fib3(n-1, memo)
-    return memo[n]
-
-
-print(fib3(10))
-
-
-# write a fibbonaci function recursive with memoization and lru_cache
