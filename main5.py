70453feef86ad2784a9e791f723c1f3efa5625f4
Kayetic <bartek.bak@protonmail.com>
2023-03-29 09:47:57 +0100

    🚀 feat(main5.py): add memoization to recursive fibonacci function

--- main5.py
+++ main5.py
@@ -19,3 +19,16 @@ def fib2(n):
     if n < 2:
         return n
     return fib2(n-2) + fib2(n-1)
+
+
+print(fib2(10))
+
+
+# write a fibbonaci function recursive with memoization
+
+def fib3(n, memo={}):
+    if n < 2:
+        return n
+    if n not in memo:
+        memo[n] = fib3(n-2, memo) + fib3(n-1, memo)
+    return memo[n]
