9468a1b3d528ad2b6df3e8094e3206ff382842a7
Kayetic <bartek.bak@protonmail.com>
2023-03-29 09:24:51 +0100

    refactor(main5.py): remove unnecessary imports and blank lines, and fix typo in function name

--- main5.py
+++ main5.py
@@ -1,3 +1,16 @@
+import openai
+import os
+import json
+import sys
+import time
+import random
+import string
+import requests
+import re
+import datetime
+import time
+
+
 # write a fibbonaci function non recursive
 
 def fib(n):
