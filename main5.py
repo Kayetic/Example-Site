d40400b5a6669b7fa603706394e05eb4a993f9c3
Kayetic <bartek.bak@protonmail.com>
2023-03-29 21:18:37 +0100

    🔨 refactor(main5.py): simplify the loop to press 'down' key ✨ feat(main5.py): add a delay of 3 seconds after every 25 'down' key presses

--- main5.py
+++ main5.py
@@ -4,8 +4,7 @@ import time
 pyautogui.PAUSE = 0.2
 pyautogui.FAILSAFE = True
 
-for i in range(1, 26):
-    pyautogui.press('down')
-    if i == 25:
-        time.sleep(3)
-        i = 0
+for i in range(12):
+    for j in range(25):
+        pyautogui.press('down')
+    time.sleep(3)
