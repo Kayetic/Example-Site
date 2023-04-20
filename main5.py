7bac388c8fd8b9f3ef6bf262fd00d38feaf5f1b6
Kayetic <bartek.bak@protonmail.com>
2023-04-20 14:09:57 +0100

    katy python start

---
+++ main5.py
@@ -0,0 +1,15 @@
+n = 10
+x = 'hello'
+y = 'world'
+# list = ['cheese', 'david tennant', 11]
+resistance = [0.3, 0.7, 0.9, 0.1, 0.2, 0.4, 0.6, 0.8, 0.5, 0.0]
+# print(x, y)
+
+# adds 2 numbers
+# print(n+5)
+
+# for x in resistance:
+#     print('resistance:', x*5)
+
+
+### taking inputs ###
