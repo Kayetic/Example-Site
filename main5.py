40d833482750f78c55d54521f36ccd320e154f5c
Kayetic <bartek.bak@protonmail.com>
2023-04-21 17:58:55 +0100

    🔥 chore(main4.py): remove OpenAI chatbot code
    🎨 style(main5.py): remove commented out code and unused variables, add input prompt and print statement

--- main5.py
+++ main5.py
@@ -1,15 +1,28 @@
-n = 10
-x = 'hello'
-y = 'world'
-# list = ['cheese', 'david tennant', 11]
-resistance = [0.3, 0.7, 0.9, 0.1, 0.2, 0.4, 0.6, 0.8, 0.5, 0.0]
+# import datetime
+# import time
+# n = 10.5
+# n = '10'
+# x = 'hello'
+# y = 'world'
+# # list = ['cheese', 'david tennant', 11]
+# resistance = [0.3, 0.7, 0.9, 0.1, 0.2, 0.4, 0.6, 0.8, 0.5, 0.0]
 # print(x, y)
 
-# adds 2 numbers
-# print(n+5)
+# # adds 2 numbers
+# # print(n+5)
+# # print((x + ' ' + y + ' ') * 3)
 
-# for x in resistance:
-#     print('resistance:', x*5)
+# x = 'David'
+# y = 'teninch'
+# z = 'dilf'
+# registered = False
 
+# print('the patient with the first name', x,
+#       'and the last name', y, 'is', z, 'years old.')
 
 ### taking inputs ###
+
+# print('who is your favourite dilf?')
+# input('who is your favourite dilf? ')
+n = input('who is your favourite dilf? ')
+print('you like', n[::-1])
